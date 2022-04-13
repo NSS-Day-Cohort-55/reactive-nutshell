9154864f6730db50eb810b646e37f90b90d26a82
Brenda Long <brenda@studio6thave.com>
2022-04-13 09:37:34 -0500

    update with react-router-dom

--- src/components/Nutshell.js
+++ src/components/Nutshell.js
@@ -1,13 +1,26 @@
-import React from "react"
+import React, {useState} from "react"
 import { ApplicationViews } from "./ApplicationViews"
 import { NavBar } from "./nav/NavBar"
 import "./Nutshell.css"
 
 export const Nutshell = () => {
-  
+  const [isAuthenticated, setIsAuthenticated] = useState(sessionStorage.getItem("nutshell_user") !== null)
+
+    const setAuthUser = (user) => {
+        sessionStorage.setItem("nutshell_user", JSON.stringify(user))
+        setIsAuthenticated(sessionStorage.getItem("nutshell_user") !== null)
+    }
+    
+    const clearUser = () => {
+        sessionStorage.clear();
+        setIsAuthenticated(sessionStorage.getItem("kennel_customer") !== null)
+      }
+    
 return (
   <>
-  <NavBar />
-  <ApplicationViews />
+  <NavBar clearUser={clearUser} isAuthenticated={isAuthenticated}/>
+  <ApplicationViews setAuthUser={setAuthUser}
+                isAuthenticated={isAuthenticated}
+                setIsAuthenticated={setIsAuthenticated}/>
   </>
 )}

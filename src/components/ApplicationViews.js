b078557c579e1a89c2349510d1aeaa8919244a90
Brenda Long <brenda@studio6thave.com>
2022-04-13 09:34:33 -0500

    update with outlet

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,15 +1,27 @@
 import React from "react"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Outlet } from "react-router-dom"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
 
-export const ApplicationViews = () => {
+export const ApplicationViews = ({isAuthenticated, setIsAuthenticated}) => {
+  const PrivateOutlet = () => {
+		return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+	}
+  
+  const setAuthUser = (user) => {
+		sessionStorage.setItem("nutshell_user", JSON.stringify(user))
+		setIsAuthenticated(sessionStorage.getItem("nutshell_user") !== null)
+	}
+  
   return (
     <>
     <Routes>
-      <Route path="/" element={} />
-      <Route path="/friends" element={} />
-      <Route path="/messages" element={} />
-      <Route path="/tasks" element={} />
-      <Route path="/events" element={} />
+      <Route path="/" element={<PrivateOutlet/>} >
+        <Route path="/friends" element={} />
+        <Route path="/messages" element={} />
+        <Route path="/tasks" element={} />
+        <Route path="/events" element={} />
+      </Route>
     </Routes>
     </>
   )

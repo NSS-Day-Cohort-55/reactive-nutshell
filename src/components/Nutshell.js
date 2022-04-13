a0bc6860de5675bc0ed3c23d283ad521009b9814
Brenda Long <brenda@studio6thave.com>
2022-04-13 09:24:09 -0500

    update based on react-router-dom

--- src/components/Nutshell.js
+++ src/components/Nutshell.js
@@ -1,33 +1,13 @@
 import React from "react"
-import { Route, Redirect } from "react-router-dom"
 import { ApplicationViews } from "./ApplicationViews"
 import { NavBar } from "./nav/NavBar"
-import { Login } from "./auth/Login"
-import { Register } from "./auth/Register"
 import "./Nutshell.css"
 
-export const Nutshell = () => (
+export const Nutshell = () => {
+  
+return (
   <>
-    <Route
-      render={() => {
-        if (sessionStorage.getItem("nutshell_user")) {
-          return (
-            <>
-              <NavBar />
-              <ApplicationViews />
-            </>
-          )
-        } else {
-          return <Redirect to="/login" />;
-        }
-      }}
-    />
-
-    <Route path="/login">
-      <Login />
-    </Route>
-    <Route path="/register">
-      <Register />
-    </Route>
+  <NavBar />
+  <ApplicationViews />
   </>
-)
+)}
